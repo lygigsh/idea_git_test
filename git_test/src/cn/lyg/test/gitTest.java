ff75a582afc9b3b33e56dcaf67f99e882e43ceae
lyg <3204235654@qq.com>
2020-06-08 11:50:18 +0800

    将git工程提交到本地仓库中

---
+++ git_test/src/cn/lyg/test/gitTest.java
@@ -0,0 +1,14 @@
+package cn.lyg.test;
+
+public class gitTest {
+    public static void main(String[] args) {
+        System.out.println("idea git test");
+        System.out.println("idea git test");
+        System.out.println("idea git test");
+        System.out.println("idea git test");
+        System.out.println("idea git test");
+        System.out.println("idea git test");
+        System.out.println("idea git test");
+    }
+
+}
